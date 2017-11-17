781b86e1b76fd22b6848e17fef5c25f145093600
Jarred Parr <jparr721@gmail.com>
2017-11-17 11:45:53 -0500

    Added data parsing, cleaned up JSON

--- RequestMapperAPI/requestMapper.hpp
+++ RequestMapperAPI/requestMapper.hpp
@@ -2,9 +2,18 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <string>
+#include <map>
+#include <algorithm>
+#include <fstream>
 
 class RequestMapper {
 	public:
 		void retreiveData(std::string);
 		void postData();
+    	static  size_t curlWriteCallback(void*, size_t, size_t, std::string*);
+    	std::string cleanUpData(std::string);
+    	std::map exportToHashmap(std::string);
+
+    	template <class KTy, class Ty>
+    	void mapStatsToFile(std::map<KTy, Ty>, std::string);
 };
