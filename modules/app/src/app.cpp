ef5461fc08beb4db055f6ca8037bc243cdaf8e07
jparr721 <jparr721@gmail.com>
2018-01-27 23:32:04 -0500

    Holy mother of christ it is finally working as intended

--- modules/app/src/app.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../../Theia/src/theia.hpp"
-
-int main(int argc, char* argv[]) {
-	Theia th;
-	th.output();
-	std::cout << "Main App Firing" << std::endl;
-	return EXIT_SUCCESS;
-}
