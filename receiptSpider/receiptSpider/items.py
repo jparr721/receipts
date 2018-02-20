e3c3b483053a3fa9261b0e52a31a1fead97466c1
jparr721 <jparr721@gmail.com>
2018-02-19 21:07:26 -0500

    Setting up web scraping, CLI-ing Theia

--- receiptSpider/receiptSpider/items.py
+++ receiptSpider/receiptSpider/items.py
@@ -1,13 +1,6 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 class ReceiptspiderItem(scrapy.Item):
-	receipUrl = scrapy.Field()
 	store = scrapy.Field()
-	data = scrapy.Field()
+	file_urls = scrapy.Field()
+	files = scrapy.Field()
