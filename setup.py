30eecc48324f59cb7bb85500d01dab49a4f29f1c
Jarred Parr <jparr721@gmail.com>
2018-04-03 15:23:15 -0400

    Refactor complete, way less gross now

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 import os
 from setuptools import setup
 
+
 def read(filename):
-	return open(os.path.join(os.path.dirname(__file__), filename)).read()
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
 
 setup(
-	name='receipt',
-	version='1.0.0',
-	author='Jarred Parr',
-	author_email='jparr721@gmail.com',
-	install_requires=['PIL', 'pytesseract', 'psycopg2'],
-	scripts=['bin/receipts'],
-	description='Simple receipt data management',
-	license='MIT',
-	long_description=read('README.md'),
-     )
+    name='receipt',
+    version='2.5.0',
+    author='Jarred Parr',
+    author_email='jparr721@gmail.com',
+    install_requires=['PIL', 'pytesseract'],
+    scripts=['bin/receipts'],
+    description='Simple receipt data management',
+    license='MIT',
+    long_description=read('README.md'),
+)
